afe6bf5e4be7613734b903e80e0779e50c99e94c
Piotr Wicijowski <piotr@wicijowski.pl>
2022-11-30 22:28:00 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DomainList from './components/DomainList';
+
+function App() {
+  return (
+    <div className="App">
+      <DomainList></DomainList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DomainList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { supabase } from '../services/supabase';
+import styles from './DomainList.module.css';
+
+function DomainList() {
+  const [domains, setDomains] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await supabase.from('Domains').select();
+      setDomains(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <ul className={styles.list}>
+      {domains.map(domain =>
+        <li className={styles.element} key={domain.id}>{domain.name}</li>
+      )}
+    </ul>
+  );
+}
+
+export default DomainList;
