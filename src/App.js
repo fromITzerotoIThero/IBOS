f51da1444318b8a9315a8aa5333869ba661024e9
fromITzerotoIThero <mpanek.uni@gmail.com>
2022-12-08 05:37:59 +0100

    Deleting the old "common" folder and it's contents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import DomainList from './components/DomainList';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <DomainList></DomainList>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <DomainList></DomainList>
+        </div>
+    );
 }
 
 export default App;
