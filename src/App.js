b867c0eabbf139d0db482ddbd80efb201e4b6eeb
fromITzerotoIThero <mpanek.uni@gmail.com>
2022-12-08 05:04:01 +0100

    Create new Header component
    Deleting excessive folders and index.js files and creating a Header that
    looks kinda good but it's not sticky.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import DomainList from './components/DomainList';
-import { Header } from './components/common';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <DomainList></DomainList>
     </div>
   );
