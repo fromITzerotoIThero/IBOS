fd7cca626eb60a0d34912e61506cd53bd20bc978
fromITzerotoIThero <mpanek.uni@gmail.com>
2022-12-12 15:27:00 +0100

    Adding more styles to WelcomeScreen

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -5,11 +5,11 @@ function WelcomeScreen() {
 
     return (
         <section className={styles.welcome_screen}>
-            <header>Welcome to IBOS</header>
-            <main>
+            <header className={styles.header}>Welcome to IBOS!</header>
+            <main className={styles.main_content}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </main>
-            <button>Learn more</button>
+            <button className={styles.button}>Learn more</button>
         </section>
     );
 
