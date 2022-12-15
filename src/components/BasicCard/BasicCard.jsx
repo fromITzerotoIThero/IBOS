0bbd01c0ac4c4eb5545974954977fdc0f6c76dc8
fromITzerotoIThero <mpanek.uni@gmail.com>
2022-12-15 22:21:13 +0100

    Naming change

--- src/components/BasicCard/BasicCard.jsx
+++ src/components/BasicCard/BasicCard.jsx
@@ -4,10 +4,10 @@ import styles from './BasicCard.module.css';
 function BasicCard() {
 
     return (
-        <div className={styles.basic_card}>
+        <div className={styles.card}>
             <div className={styles.title}>Title</div>
             <div className={styles.body}>Description</div>
-            <a href="#"><button className={styles.button} >Button text</button></a>
+            <a href="#" className={styles.button}>Button text</a>
         </div>
     );
 
