81336c0e5a8c7bf7b533693b7ea2b255fc66a42f
fromITzerotoIThero <mpanek.uni@gmail.com>
2022-12-21 12:43:38 +0100

    WelcomeScreen as a container for BasicCard

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import styles from './WelcomeScreen.module.css';
+import BasicCard from '../BasicCard/BasicCard';
 
 function WelcomeScreen({ title, description, buttonText, buttonLink }) {
 
     return (
-        <div className={styles.welcome_screen}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.description}>
-                {description}
-            </div>
-            <a href={buttonLink} className={styles.button}>{buttonText}</a>
+        <div>
+
+            <BasicCard
+                title={title}
+                description={description}
+                buttonText={buttonText}
+                buttonLink={buttonLink}
+            />
+
         </div>
     );
 
