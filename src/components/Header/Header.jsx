753c673c323b5a2499a0a0fcacf6e1c6373c729d
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-01-10 14:11:33 +0100

    Adding HomePage icon to navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 function Header() {
 
     return (
         <div className={styles.header}>
-            <Link to="/" className={styles.navbar__entry}>Welcome</Link>
+            <Link to="/" className={styles.navbar__entry}><span><HomeOutlinedIcon />Welcome</span></Link>
             <Link to="/domainsoverview" className={styles.navbar__entry}>Domains</Link>
             <Link to="/domainlist" className={styles.navbar__entry}>Domain List</Link>
             <Link to="/dummypage" className={styles.navbar__entry}>DummyPage</Link>
