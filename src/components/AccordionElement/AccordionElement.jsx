cb3722bb09121c3970cac4fb80a40b9b3f7647b0
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-01-26 13:21:41 +0100

    Applied classNames npm to AccordionElement

--- src/components/AccordionElement/AccordionElement.jsx
+++ src/components/AccordionElement/AccordionElement.jsx
@@ -2,28 +2,36 @@ import React, { useState } from 'react';
 import styles from './AccordionElement.module.css';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import classNames from 'classnames';
 
 function AccordionElement({ talentName, talentShortDescription }) {
 
     const [expanded, setExpanded] = useState(false);
     const toggleAccordion = () => {
-        setExpanded((expanded) => !expanded);
+        setExpanded(!expanded);
     };
 
     return (
-        <div className={expanded ? styles.accordion__element__expanded : styles.accordion__element}>
+        <div className={ classNames ( styles.accordion__element, { [styles.accordion__element__expanded]: expanded } )}>
             <div className={styles.title} onClick={toggleAccordion}>
                 <div className={styles.title__text}>{talentName}</div>
-                <div class name={styles.icon}>{expanded ? <ExpandLessIcon /> : <ExpandMoreIcon/>}</div>
+                <div className={styles.icon}>{expanded ? <ExpandLessIcon /> : <ExpandMoreIcon/>}</div>
             </div>
-            {expanded && (
+            <div className={ classNames( { [styles.content]: expanded }, { [styles.content__collapsed]: !expanded } )}>
+                {talentShortDescription}
+            </div>
+
+            {/* {expanded && (
                 <div className={styles.content}>
                 {talentShortDescription}
                 </div>
-            )}
+            )} */}
+
         </div>
     );
 
 }
 
 export default AccordionElement;
+
+
