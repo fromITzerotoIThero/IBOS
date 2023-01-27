88c58f55596ef80899dd968fa8190e82aee7a265
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-01-27 10:44:16 +0100

    AccordionElement complete with simplified CSS

--- src/components/AccordionElement/AccordionElement.jsx
+++ src/components/AccordionElement/AccordionElement.jsx
@@ -12,7 +12,7 @@ function AccordionElement({ talentName, talentShortDescription }) {
     };
 
     return (
-        <div className={ classNames ( styles.accordion__element, { [styles.accordion__element__expanded]: expanded } )}>
+        <div className={ classNames ( styles.collapsed, { [styles.expanded]: expanded } )}>
             <div className={styles.title} onClick={toggleAccordion}>
                 <div className={styles.title__text}>{talentName}</div>
                 <div className={styles.icon}>{expanded ? <ExpandLessIcon /> : <ExpandMoreIcon/>}</div>
@@ -20,20 +20,9 @@ function AccordionElement({ talentName, talentShortDescription }) {
             <div className={ classNames( { [styles.content]: expanded }, { [styles.content__collapsed]: !expanded } )}>
                 {talentShortDescription}
             </div>
-
-            {/* {expanded && (
-                <div className={styles.content}>
-                {talentShortDescription}
-                </div>
-            )} */}
-
         </div>
     );
 
 }
 
 export default AccordionElement;
-
-
-
-
