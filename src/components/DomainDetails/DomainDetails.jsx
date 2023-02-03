7216115f00968d7b1dbe6738dd41c7631b7c5539
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-03 11:18:03 +0100

    DomainDetails nearly there

--- src/components/DomainDetails/DomainDetails.jsx
+++ src/components/DomainDetails/DomainDetails.jsx
@@ -2,43 +2,32 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../services/supabase';
 import styles from './DomainDetails.module.css';
+import AccordionElement from '../AccordionElement/AccordionElement';
 
 function DomainDetails() {
 
-    const [themes, setThemes] = useState([]);
-    const [domain, setDomain] = useState([])
+    const [domain, setDomain] = useState({ Themes: [] });
 
     useEffect(() => {
-      async function fetchData() {
-        const { data } = await supabase
-            .from('Themes')
-            .select()
-            .eq('domain_id', '70a7d207-499f-4973-bb29-458af6f3a1f9')
-            // .select('name', 'Domains' ('name'))
-            // .order('domain_id')
+        async function fetchData() {
+            const { data } = await supabase
+                .from('Domains')
+                .select('*, Themes(*)')
+                .eq('id', '70a7d207-499f-4973-bb29-458af6f3a1f9')
 
-        setThemes(data);
-        // setDomains(data);
-      }
-      fetchData();
+            setDomain(data[0]);
+        }
+        fetchData();
     }, []);
 
     return (
-        <ul className={styles.list}>
-            {/* {domains.map(domain =>
-                <li className={styles.element} key={domain.id}>{domain.name}</li>
-            )} */}
-            {themes.map(theme =>
-                <div>
-                    <li className={styles.element} key={theme.id}>
-                        <div>{theme.name}</div>
-                        <div>{theme.id}</div>
-                        <div>{theme.domain_id}</div>
-                        {/* <div>{theme.description}</div> */}
-                    </li>
-                </div>
+        <div className={styles.container}>
+            <div className={styles.title}>{domain.name}</div>
+            <div className={styles.details}>{domain.details}</div>
+            {domain.Themes.map(theme =>
+                <AccordionElement themeName={theme.name} themeDescription={theme.description} key={theme.id} />
             )}
-        </ul>
+        </div>
     );
 }
 
