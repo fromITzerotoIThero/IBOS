02dcfa04c2c9b92c9754c15f99756dbe4deba53c
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-08 15:13:03 +0100

    DomainHeader with no data fetching

--- src/components/DomainHeader/DomainHeader.jsx
+++ src/components/DomainHeader/DomainHeader.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import { supabase } from '../../services/supabase';
 import styles from './DomainHeader.module.css';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Link } from 'react-router-dom';
 
-function DomainHeader() {
-
-    const [domainName, setDomainName] = useState([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const { data } = await supabase
-                .from('Domains')
-                .select('name')
-                // .eq('id', '70a7d207-499f-4973-bb29-458af6f3a1f9')
-            setDomainName(data);
-        }
-        fetchData();
-    }, []);
+function DomainHeader({ backwardLink, domainName }) {
 
     return (
-        <div className={styles.container}>
-            {domainName.map(domainName =>
-                <div className={styles.title} domainName={domainName.name} key={domainName.id}><Link to="/domainsoverview"><ChevronLeftIcon className={styles.icon}/></Link>{domainName.name}</div>
-            )}
+        <div className={styles.title}>
+            <Link to={backwardLink}><ChevronLeftIcon className={styles.icon}/></Link>
+            {domainName}
         </div>
     )
+
 }
 
 export default DomainHeader;
