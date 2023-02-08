a77d0a650fa3cf84976653a97e42bbb63e70784a
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-08 15:16:19 +0100

    DomainList and DomainsOverview cosmetic changes

--- src/components/DomainList/DomainList.jsx
+++ src/components/DomainList/DomainList.jsx
@@ -4,22 +4,24 @@ import { supabase } from '../../services/supabase';
 import styles from './DomainList.module.css';
 
 function DomainList() {
-  const [domains, setDomains] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      const { data } = await supabase.from('Domains').select();
-      setDomains(data);
-    }
-    fetchData();
-  }, []);
 
-  return (
-    <ul className={styles.list}>
-      {domains.map(domain =>
-        <li className={styles.element} key={domain.id}>{domain.name}</li>
-      )}
-    </ul>
-  );
+    const [domains, setDomains] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await supabase.from('Domains').select();
+            setDomains(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <ul className={styles.list}>
+            {domains.map(domain =>
+                <li className={styles.element} key={domain.id}>{domain.name}</li>
+            )}
+        </ul>
+    );
 }
 
 export default DomainList;
--- src/components/DomainsOverview/DomainsOverview.jsx
+++ src/components/DomainsOverview/DomainsOverview.jsx
@@ -3,24 +3,27 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../../services/supabase';
 import BasicCard from '../BasicCard/BasicCard';
 
+
 function DomainsOverview() {
 
     const [domains, setDomains] = useState([]);
 
     useEffect(() => {
-      async function fetchData() {
-        const { data } = await supabase.from('Domains').select();
-        setDomains(data);
-      }
-      fetchData();
+        async function fetchData() {
+            const { data } = await supabase
+                .from('Domains')
+                .select();
+            setDomains(data);
+        }
+        fetchData();
     }, []);
 
     return (
-      <div>
-        {domains.map(domain =>
-            <BasicCard title={domain.name} description={domain.short_description} buttonText="Learn more" buttonLink="/domaindetails"/>
-        )}
-      </div>
+        <div>
+            {domains.map(domain =>
+                <BasicCard title={domain.name} description={domain.short_description} buttonText="Learn more" buttonLink="/domaindetails" />
+            )}
+        </div>
     );
 }
 
