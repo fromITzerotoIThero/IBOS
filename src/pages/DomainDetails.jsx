e1b47636d8c7ac1a58f32054994f63feb11e75c5
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-08 15:17:13 +0100

    DomainDetails page nearly done

---
+++ src/pages/DomainDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { supabase } from '../services/supabase';
+import AccordionElement from '../components/AccordionElement/AccordionElement';
+import styles from './DomainDetails.module.css';
+import DomainHeader from '../components/DomainHeader/DomainHeader';
+
+function DomainDetails() {
+
+    const [domain, setDomain] = useState({ Themes: [] });
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await supabase
+                .from('Domains')
+                .select('*, Themes(*)')
+                // .eq('id', 'Domains.id')
+
+            setDomain(data[0]);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <DomainHeader backwardLink="/domainsoverview" domainName={domain.name} key={domain.id}/>
+            <div className={styles.details}>{domain.details}</div>
+            <div>
+                {domain.Themes.map(theme =>
+                    <AccordionElement themeName={theme.name} themeDescription={theme.description} key={theme.id} />
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default DomainDetails;
