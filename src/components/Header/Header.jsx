8b2e05ad35d78d562c57b328216038da2f6b85c4
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-17 15:19:44 +0100

    Header navigation switched to NavLinks - doesn't work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import ListIcon from '@mui/icons-material/List';
 
@@ -8,9 +8,9 @@ function Header() {
 
     return (
         <div className={styles.header}>
-            <Link to="/" className={styles.navbar__entry}><HomeOutlinedIcon className={styles.navbar__icon}/>Welcome</Link>
-            <Link to="/domainsoverview" className={styles.navbar__entry}><ListIcon className={styles.navbar__icon}/>Domains</Link>
-            <Link to="/domainlist" className={styles.navbar__entry}>Domain List</Link>
+            <NavLink to="/" activeClassName={styles.active} className={styles.navbar__entry}><HomeOutlinedIcon className={styles.navbar__icon} />Welcome</NavLink>
+            <NavLink to="/domainsoverview" className={styles.navbar__entry}><ListIcon className={styles.navbar__icon} />Domains</NavLink>
+            <NavLink to="/domainlist" className={styles.navbar__entry}>Domain List</NavLink>
         </div>
     );
 
