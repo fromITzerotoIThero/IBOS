9ef473bbcffbc35d18fc594f90d10c195426a787
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-02-28 14:56:57 +0100

    NavBarEntry - adding active class with NavLink

--- src/components/NavBarEntry/NavBarEntry.jsx
+++ src/components/NavBarEntry/NavBarEntry.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './NavBarEntry.module.css';
 import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 function NavBarEntry({ targetName, entryName, icon }) {
 
     return (
         <NavLink
             to={targetName}
-            className={styles.navbar__entry}>
-                {icon}
-                {entryName}
+            className={classNames(styles.navbar__entry, [({ isActive }) => isActive ? styles.active : "" ])}>
+            {icon}
+            {entryName}
         </NavLink>
-        );
+    );
 }
 
 export default NavBarEntry;
