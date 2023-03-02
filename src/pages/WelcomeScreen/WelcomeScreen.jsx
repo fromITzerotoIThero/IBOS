261387e4cc49ba61257a0cbcb54da6e67bcdc8f3
Piotr Wicijowski <piotr@wicijowski.pl>
2023-03-02 22:52:21 +0100

    Working implementation of closing welcome card from welcome screen

--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import BasicCard from '../../components/BasicCard/BasicCard';
 
 function WelcomeScreen() {
 
-    // const [showWelcomeScreen, setShowWelcomeScreen] = useState(
-    //     !localStorage.getItem("hasClosedWelcomeScreen")
-    // );
+    //this state is string, because it is taken from localStorage
+    const myStateArray = useState(
+        localStorage.getItem("hasClosedWelcomeScreen")
+    );
+    const hasClosedWelcomeScreen = myStateArray[0]
+    const setClosedWelcomeScreen = myStateArray[1]
 
-    // const handleWelcomeScreenClose = () => {
-    //     localStorage.setItem("hasClosedWelcomeScreen", true);
-    //     setShowWelcomeScreen(false);
-    // };
+    let showWelcomeScreen = shouldIShow(hasClosedWelcomeScreen);
+
+    const handleWelcomeScreenClose = () => {
+        localStorage.setItem("hasClosedWelcomeScreen", "true");
+        //make sure to set the state as string, because it is for local storage
+        setClosedWelcomeScreen("true");
+    };
 
     return (
         <div>
+            {showWelcomeScreen && (
             <BasicCard
                 title="Welcome to IBOS"
                 description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -21,7 +29,8 @@ function WelcomeScreen() {
                 buttonLink="/domainsoverview"
                 showCloseButton={true}
                 ariaLabel="Close welcome screen"
-            />
+                onClose={handleWelcomeScreenClose}
+            />)}
 
         </div>
     );
@@ -29,3 +38,39 @@ function WelcomeScreen() {
 }
 
 export default WelcomeScreen;
+
+
+// const myStateArray = useState(
+//     localStorage.getItem("hasClosedWelcomeScreen")
+// );
+
+// let hasClosedWelcomeScreen = myStateArray[0]
+// let showWelcomeScreen = true;
+
+// if(hasClosedWelcomeScreen == null) {
+//     showWelcomeScreen = true
+// }
+// if(hasClosedWelcomeScreen == "true") {
+//     showWelcomeScreen = false
+// }
+// const setClosedWelcomeScreen = myStateArray[1]
+
+// const handleWelcomeScreenClose = () => {
+//     localStorage.setItem("hasClosedWelcomeScreen", "true");
+//     setClosedWelcomeScreen("true");
+// };
+
+function shouldIShow(hasClosedWelcomeScreen){
+    let result = true;
+
+    //first render before we close it
+    if(hasClosedWelcomeScreen == null) {
+        result = true
+    }
+
+    if(hasClosedWelcomeScreen == "true") {
+        result = false
+    }
+
+    return result;
+}
