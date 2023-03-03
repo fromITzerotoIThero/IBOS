c8299f943dc04b4634cc3093013717c0168451a2
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-03-03 14:23:00 +0100

    Different version of WelcomeScreen dismissed

--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -4,33 +4,30 @@ import BasicCard from '../../components/BasicCard/BasicCard';
 
 function WelcomeScreen() {
 
-    //this state is string, because it is taken from localStorage
-    const myStateArray = useState(
-        localStorage.getItem("hasClosedWelcomeScreen")
+    const [ visibleWelcomeScreen, setVisibleWelcomeScreen ] = useState(
+        localStorage.getItem("visibleWelcomeScreen")
     );
-    const hasClosedWelcomeScreen = myStateArray[0]
-    const setClosedWelcomeScreen = myStateArray[1]
 
-    let showWelcomeScreen = shouldIShow(hasClosedWelcomeScreen);
+    let showWelcomeScreen = shouldShow(visibleWelcomeScreen);
 
     const handleWelcomeScreenClose = () => {
-        localStorage.setItem("hasClosedWelcomeScreen", "true");
-        //make sure to set the state as string, because it is for local storage
-        setClosedWelcomeScreen("true");
+        localStorage.setItem("visibleWelcomeScreen", "false");
+        setVisibleWelcomeScreen("false");
     };
+
 
     return (
         <div>
             {showWelcomeScreen && (
-            <BasicCard
-                title="Welcome to IBOS"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                buttonText="Learn more"
-                buttonLink="/domainsoverview"
-                showCloseButton={true}
-                ariaLabel="Close welcome screen"
-                onClose={handleWelcomeScreenClose}
-            />)}
+                <BasicCard
+                    title="Welcome to IBOS"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    buttonText="Learn more"
+                    buttonLink="/domainsoverview"
+                    showCloseButton={true}
+                    ariaLabel="Close welcome screen"
+                    onClose={handleWelcomeScreenClose}
+                />)}
 
         </div>
     );
@@ -40,37 +37,69 @@ function WelcomeScreen() {
 export default WelcomeScreen;
 
 
-// const myStateArray = useState(
-//     localStorage.getItem("hasClosedWelcomeScreen")
-// );
-
-// let hasClosedWelcomeScreen = myStateArray[0]
-// let showWelcomeScreen = true;
-
-// if(hasClosedWelcomeScreen == null) {
-//     showWelcomeScreen = true
-// }
-// if(hasClosedWelcomeScreen == "true") {
-//     showWelcomeScreen = false
-// }
-// const setClosedWelcomeScreen = myStateArray[1]
-
-// const handleWelcomeScreenClose = () => {
-//     localStorage.setItem("hasClosedWelcomeScreen", "true");
-//     setClosedWelcomeScreen("true");
-// };
-
-function shouldIShow(hasClosedWelcomeScreen){
+function shouldShow(visibleWelcomeScreen) {
     let result = true;
 
     //first render before we close it
-    if(hasClosedWelcomeScreen == null) {
-        result = true
+    if (visibleWelcomeScreen == "true") {
+        result = true;
     }
 
-    if(hasClosedWelcomeScreen == "true") {
-        result = false
+    if (visibleWelcomeScreen == "false") {
+        result = false;
     }
 
     return result;
 }
+
+
+// function WelcomeScreen() {
+
+//     const [ closedWelcomeScreen, setClosedWelcomeScreen ] = useState(
+//         localStorage.getItem("closedWelcomeScreen")
+//     );
+
+//     let showWelcomeScreen = shouldShow(closedWelcomeScreen);
+
+//     const handleWelcomeScreenClose = () => {
+//         localStorage.setItem("closedWelcomeScreen", "true");
+//         setClosedWelcomeScreen("true");
+//     };
+
+
+//     return (
+//         <div>
+//             {showWelcomeScreen && (
+//                 <BasicCard
+//                     title="Welcome to IBOS"
+//                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//                     buttonText="Learn more"
+//                     buttonLink="/domainsoverview"
+//                     showCloseButton={true}
+//                     ariaLabel="Close welcome screen"
+//                     onClose={handleWelcomeScreenClose}
+//                 />)}
+
+//         </div>
+//     );
+
+// }
+
+// export default WelcomeScreen;
+
+
+// function shouldShow(closedWelcomeScreen) {
+//     let result = true;
+
+//     //first render before we close it
+//     if (closedWelcomeScreen == null) {
+//         result = true;
+//     }
+
+//     if (closedWelcomeScreen == "true") {
+//         result = false;
+//     }
+
+//     return result;
+// }
+
