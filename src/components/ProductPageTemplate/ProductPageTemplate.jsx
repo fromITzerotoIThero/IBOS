1a4b4914a84fe0f10bb66fac450705ab3b7b02eb
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-03-30 14:59:09 +0200

    Created ProductPageTemplate component

---
+++ src/components/ProductPageTemplate/ProductPageTemplate.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './ProductPageTemplate.module.css';
+
+function ProductPageTemplate({ productName, isDiscounted, originalPrice, currentPrice, productDescription }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div classname={styles.name}>
+                    {productName}
+                </div>
+                <div>
+                    <div className={styles.price_row}>
+                        {isDiscounted === true
+                            ?
+                            <div>
+                                ${currentPrice}
+                                <span className={styles.before_discount}>${originalPrice}</span>
+                            </div>
+                            :
+                            <div>${originalPrice}</div>
+                        }
+                    </div>
+                    <button className={styles.cart_btn}>
+                    </button>
+                </div>
+            </div>
+            <div>
+                {/* <ImageCarousel /> */}
+            </div>
+            <div className={styles.description}>
+                {productDescription}
+            </div>
+        </div>
+    )
+}
+
+export default ProductPageTemplate;
