a30828715f8f3c9889fc383a87bd60ea07af29f2
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-04-06 22:21:58 +0200

    Cleaning up ProductPage mess

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { supabase } from '../../services/supabase';
-import ProductPageTemplate from '../../components/ProductPageTemplate/ProductPageTemplate';
 import styles from './ProductPage.module.css';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function ProductPage() {
 
     const { id } = useParams();
     const [product, setProduct] = useState(null);
+
+    const isDiscounted = false;
+    const currentPrice = null;
 
     useEffect(() => {
         async function fetchData() {
@@ -20,20 +23,43 @@ function ProductPage() {
             setProduct(data[0]);
         }
         fetchData();
-    }, []);
+    }, [id]);
 
 
     return (
         <div>
             {product != null
                 ?
-                <ProductPageTemplate
-                    productName={product.name}
-                    originalPrice={product.price}
-                    productDescription={product.description}
-                    imgArray={product.thumbnail_urls}
-                    imgAlt={product.name}
-                />
+                <div className={styles.container}>
+                    <div className={styles.header}>
+                        <div className={styles.name}>
+                            {product.name}
+                        </div>
+                        <div>
+                            <div className={styles.price_row}>
+                                {isDiscounted === true
+                                    ?
+                                    <div>
+                                        ${currentPrice}
+                                        <span className={styles.before_discount}>${product.price}</span>
+                                    </div>
+                                    :
+                                    <div>${product.price}</div>
+                                }
+                            </div>
+                            <button className={styles.cart_btn}>
+                                <span>Add to cart</span>
+                                <ShoppingCartOutlinedIcon className={styles.cart_icon} />
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={product.thumbnail_urls} alt={product.name} />
+                    </div>
+                    <div className={styles.description}>
+                        {product.description}
+                    </div>
+                </div>
                 :
                 <div className={styles.loading}>Loading...</div>
             }
