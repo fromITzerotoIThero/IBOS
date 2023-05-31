a9dc80d5904552c1f0bc08725e1a71193f0ad1ec
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-05-31 14:44:32 +0200

    Created ImageCarousel with a random image array

--- src/components/ImageCarousel/ImageCarousel.jsx
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -4,21 +4,31 @@ import styles from './ImageCarousel.module.css';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CropOriginalRoundedIcon from '@mui/icons-material/CropOriginalRounded';
-import ImageIndicator from '../ImageIndicator/ImageIndicator';
+// import ImageIndicator from '../ImageIndicator/ImageIndicator';
+import { RadioButtonUncheckedOutlined } from '@mui/icons-material';
 
-function ImageCarousel({ imgArray, imgAlt }) {
+function ImageCarousel() {
+
+    const imgArray = [
+        { url: "https://images.unsplash.com/photo-1677833766807-5598cc321858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80" },
+        { url: "https://images.unsplash.com/photo-1684057044985-6cb9a99b4663?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80" },
+        { url: "https://images.unsplash.com/photo-1684051489159-526760dbb8ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80" },
+    ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const imgUrl = imgArray[currentIndex];
+    const imgUrl = imgArray[currentIndex].url;
+
+    const isFirstImg = currentIndex === 0;
+    const isLastImg = currentIndex === imgArray.length - 1;
+    const leftArrow = isFirstImg ? styles.hidden : styles.arrow;
+    const rightArrow = isLastImg ? styles.hidden : styles.arrow;
 
     const prevImg = () => {
-        const isFirstImg = currentIndex === 0;
         const newIndex = isFirstImg ? imgArray.length - 1 : currentIndex - 1;
         setCurrentIndex(newIndex);
     };
 
     const nextImg = () => {
-        const isLastImg = currentIndex === imgArray.length - 1;
         const newIndex = isLastImg ? imgArray.length - 1 : currentIndex + 1;
         setCurrentIndex(newIndex);
     };
@@ -28,18 +38,20 @@ function ImageCarousel({ imgArray, imgAlt }) {
             {imgArray != null
                 ?
                 <div>
-                    <img src={imgUrl} alt={imgAlt} className={styles.img} />
-                    <div>
-                        <ChevronLeftIcon onClick={prevImg} />
-                    </div>
-                    <div>
-                        <ChevronRightIcon onClick={nextImg} />
-                    </div>
-                    <div>
-                        {imgArray.map((img, imgIndex) => (
-                            <ImageIndicator />
-                        ))}
-                    </div>
+                    <img src={imgUrl} className={styles.img} alt={""} />
+
+                    {imgArray.length > 1 && (
+                        <div className={styles.arrow_container}>
+                            <ChevronLeftIcon className={leftArrow} onClick={prevImg} />
+                            <ChevronRightIcon className={rightArrow} onClick={nextImg} />
+
+                            <div className={styles.img_indicator}>
+                                {imgArray.map(img => (
+                                    <div key={img.url}><RadioButtonUncheckedOutlined /></div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
                 </div>
                 :
                 <CropOriginalRoundedIcon className={styles.img_icon} />
@@ -47,6 +59,5 @@ function ImageCarousel({ imgArray, imgAlt }) {
         </div>
     );
 }
-
 
 export default ImageCarousel;
