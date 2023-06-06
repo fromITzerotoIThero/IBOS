9306bd5efa307cdd0b284189dda030cb5a7daa2e
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-06-06 11:24:27 +0200

    PR changes for ImageIndicator

--- src/components/ImageIndicator/ImageIndicator.jsx
+++ src/components/ImageIndicator/ImageIndicator.jsx
@@ -5,7 +5,7 @@ import CircleIcon from '@mui/icons-material/Circle';
 function ImageIndicator({ isActive, onDotClick }) {
 
     return (
-        <div className={styles.img_indicator}>
+        <div className={styles.indicator}>
             <CircleIcon
                 className={`${styles.dot} ${isActive ? styles.selected : ''}`}
                 onClick={onDotClick}
