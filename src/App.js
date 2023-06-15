0421cdcc3b5946a985ab6249e3d93199b1eb3811
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-06-15 15:26:32 +0200

    Remove test Counter from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Counter from './features/counter/Counter';
 import Header from './components/Header/Header';
 import Accordion from './components/Accordion/Accordion';
 import DomainsOverview from './components/DomainsOverview/DomainsOverview';
@@ -23,7 +22,6 @@ function App() {
     return (
         <div>
             <Header />
-            <Counter />
             <Routes>
                 <Route
                     path="/"
