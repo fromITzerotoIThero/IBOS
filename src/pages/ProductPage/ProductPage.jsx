01b40bca4e8a63a211027e741a8f955e59aec62f
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-06-15 15:27:40 +0200

    Add ShoppingCartIndicator to ProductPage

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { supabase } from '../../services/supabase';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from '../../features/counter/counterSlice';
 import styles from './ProductPage.module.css';
+import ShoppingCartIndicator from '../../components/ShoppingCartIndicator/ShoppingCartIndicator';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 
 function ProductPage() {
 
@@ -12,6 +16,9 @@ function ProductPage() {
 
     const isDiscounted = false;
     const currentPrice = null;
+
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch();
 
     useEffect(() => {
         async function fetchData() {
@@ -30,36 +37,44 @@ function ProductPage() {
         <div>
             {product != null
                 ?
-                <div className={styles.container}>
-                    <div className={styles.header}>
-                        <div className={styles.name}>
-                            {product.name}
-                        </div>
-                        <div>
-                            <div className={styles.price_row}>
-                                {isDiscounted === true
-                                    ?
-                                    <div>
-                                        ${currentPrice}
-                                        <span className={styles.before_discount}>${product.price}</span>
-                                    </div>
-                                    :
-                                    <div>${product.price}</div>
-                                }
+                <>
+                    <ShoppingCartIndicator
+                        count={count}
+                    />
+                    <div className={styles.container}>
+                        <div className={styles.header}>
+                            <div className={styles.name}>
+                                {product.name}
                             </div>
-                            <button className={styles.cart_btn}>
-                                <span>Add to cart</span>
-                                <ShoppingCartOutlinedIcon className={styles.cart_icon} />
-                            </button>
+                            <div>
+                                <div className={styles.price_row}>
+                                    {isDiscounted === true
+                                        ?
+                                        <div>
+                                            ${currentPrice}
+                                            <span className={styles.before_discount}>${product.price}</span>
+                                        </div>
+                                        :
+                                        <div>${product.price}</div>
+                                    }
+                                </div>
+                                <button
+                                    className={styles.cart_btn}
+                                    onClick={() => dispatch(increment())}
+                                >
+                                    <span>Add to cart</span>
+                                    <ShoppingCartOutlinedIcon className={styles.cart_icon} />
+                                </button>
+                            </div>
+                        </div>
+                        <div className={styles.img_container}>
+                            <img src={product.thumbnail_urls[0]} alt={product.name} />
+                        </div>
+                        <div className={styles.description}>
+                            {product.description}
                         </div>
                     </div>
-                    <div className={styles.img_container}>
-                        <img src={product.thumbnail_urls[0]} alt={product.name} />
-                    </div>
-                    <div className={styles.description}>
-                        {product.description}
-                    </div>
-                </div>
+                </>
                 :
                 <div className={styles.loading}>Loading...</div>
             }
