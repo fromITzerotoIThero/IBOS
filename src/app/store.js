6863e258d4509de0d58f4d92a3ef53d071fa3971
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-11-22 11:31:14 +0100

    Restructure Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import shoppingCartReducer from '../features/ShoppingCart/shoppingCartSlice';
+import shoppingCartReducer from './shoppingCartSlice';
 
 const store = configureStore({
     reducer: {
