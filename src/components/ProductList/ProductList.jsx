1a7c482b29197e5743430c8ee5c81786c74d6c4f
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-12-13 16:12:47 +0100

    Create simple pagination (no Redux, no RTX Query yet)

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -8,30 +8,44 @@ import ProductListItem from '../ProductListItem/ProductListItem';
 function ProductList() {
 
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 10;
 
     useEffect(() => {
         async function fetchData() {
             const { data } = await supabase
                 .from('Products')
-                .select();
+                .select()
+                .range((currentPage - 1) * productsPerPage, currentPage * productsPerPage - 1);
             setProducts(data);
         }
         fetchData();
-    }, []);
+    }, [currentPage]);
+
+    const handlePageChange = (newPage) => {
+        console.log(newPage);
+        setCurrentPage(newPage)
+    };
 
     return (
-        <div className={styles.container}>
-            {products.map(product =>
-                <div key={product.id}>
-                    <ProductListItem
-                        targetProduct={`/productpage/${product.id}`}
-                        imgSrc={product.thumbnail_urls}
-                        productName={product.name}
-                        originalPrice={product.price}
-                    />
-                </div>
-            )}
-        </div>
+        <>
+            <div className={styles.container}>
+                {products.map(product =>
+                    <div key={product.id}>
+                        <ProductListItem
+                            targetProduct={`/productpage/${product.id}`}
+                            imgSrc={product.thumbnail_urls}
+                            productName={product.name}
+                            originalPrice={product.price}
+                        />
+                    </div>
+                )}
+            </div>
+            <div>
+                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+            </div>
+        </>
     );
 }
 
