50ffce69d7c461737b9dc4756b629fccd74a67a8
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-12-14 16:13:42 +0100

    Create draft productsSlice for ProductList state management

---
+++ src/app/productsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const productsSlice = createSlice({
+    name: 'products',
+    initialState: {
+        data: [],
+        currentPage: 1,
+        totalPages: 1,
+    },
+    reducers: {
+        setProducts: (state, action) => {
+            state.data = action.payload;
+        },
+        setCurrentPage: (state, action) => {
+            state.currentPage = action.payload;
+        },
+        setTotalPages: (state, action) => {
+            state.totalPages = action.payload;
+        },
+    },
+});
+
+export const { setProducts, setCurrentPage, setTotalPages } = productsSlice.actions;
+
+export default productsSlice.reducer;
