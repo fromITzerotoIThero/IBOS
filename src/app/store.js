359b3291a195aed0c0cf45c7e9057058b6602bc2
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-12-15 15:09:48 +0100

    Add productsApi  to Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { productsApi } from '../services/productsApi';
 import shoppingCartReducer from './shoppingCartSlice';
 
 const store = configureStore({
     reducer: {
+        [productsApi.reducerPath]: productsApi.reducer,
         shoppingCart: shoppingCartReducer,
     },
 });
+
+setupListeners(store.dispatch);
 
 export default store;
