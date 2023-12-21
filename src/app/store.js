909d53f5c4eabb7bba24afa24dc4fc27fcfba359
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-12-21 13:54:44 +0100

    Remove draft ProductList state management with Redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { productsApi } from '../services/productsApi';
 import shoppingCartReducer from './shoppingCartSlice';
 
 const store = configureStore({
     reducer: {
-        [productsApi.reducerPath]: productsApi.reducer,
         shoppingCart: shoppingCartReducer,
     },
 });
-
-setupListeners(store.dispatch);
 
 export default store;
