28a8c0d19262b4ddb2f3a10c4e490850d1ba7fb8
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-12-28 19:42:52 +0100

    Refactor DomainHeader - optimize layout

--- src/components/DomainHeader/DomainHeader.jsx
+++ src/components/DomainHeader/DomainHeader.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './DomainHeader.module.css';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Link } from 'react-router-dom';
 
 function DomainHeader({ backwardLink, domainName }) {
 
     return (
         <div className={styles.title}>
             <Link to={backwardLink}><ChevronLeftIcon className={styles.icon} /></Link>
-            {domainName}
+            <div>
+                {domainName}
+            </div>
         </div>
 
     )
